ad383ec2427b010631796fd9a61c26730599c33d
Juan Collado <juancollado.dev@gmail.com>
2020-06-05 12:04:24 -0300

    Add sockets events to recive and send message and location

--- routers/chat.js
+++ routers/chat.js
@@ -1,12 +1,31 @@
 const path = require('path')
 const express = require('express')
 const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    socket.emit('newMessage', 'Welcome to the chat')
+    socket.broadcast.emit('newMessage', 'A new user has joined!')
+
+    socket.on('clientMessage', (ms) => {
+        io.emit('newMessage', ms)
+    })
+
+    socket.on('sendLocation', (location) => {
+        socket.broadcast.emit('newMessage', `https://google.com/maps?q=${location.latitude},${location.longitude}`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('newMessage', 'An user has left!')
+    })
+})
 
 module.exports = server

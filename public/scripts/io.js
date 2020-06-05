38660bddb5fca9369b86d8006231b8db95d723bf
Juan Collado <juancollado.dev@gmail.com>
2020-06-05 12:05:24 -0300

    Add sockets eventes in front-end and get the current location

---
+++ public/scripts/io.js
@@ -0,0 +1,28 @@
+const socket = io() 
+
+socket.on('newMessage', (message) => {
+    console.log(message)
+})
+
+const plus = document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const ms = e.target.elements.message.value
+    console.log('sending...');
+    socket.emit('clientMessage', ms)
+    e.target.elements.message.value = ''
+})
+
+document.querySelector('#location-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    if (!navigator.geolocation) {
+        return console.log(`YOUR BROWSER DON'T SUPPORT GEOLOCATION`)
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})

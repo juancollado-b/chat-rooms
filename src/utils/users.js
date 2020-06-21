37fc804864c88eced6ac6f9d4527da2d931d744f
Juan Collado <juancollado.dev@gmail.com>
2020-06-21 12:04:57 -0300

    Add users logic

---
+++ src/utils/users.js
@@ -0,0 +1,43 @@
+let users = []
+
+const addUser = ({id, username, room}) => {
+    if (!username || !room) {
+        return {error: 'Username and room are required'}
+    }
+
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    if (existingUser) {
+        return {error: `Username ${username} is alredy used in ${room}'s room`}
+    }
+
+    const user = {id, username, room}
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+
+    return {error: `No users found with id "${id}"`}
+}
+
+const getUser = (id) => users.find((usr) => usr.id === id)
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room.trim().toLowerCase())
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
